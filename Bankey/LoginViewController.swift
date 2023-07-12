7066c72333a288257b1be76f23d0fa9fd2bfcce0
kierancrown <kieran.crown@gmail.com>
2023-07-12 17:33:03 +0100

    Added delegates/VC transition

--- Bankey/LoginViewController.swift
+++ Bankey/LoginViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol LoginViewControllerDelegate: AnyObject {
+    func didLogin()
+}
+
 class LoginViewController: UIViewController {
 
     let titleLabel = UILabel()
@@ -14,6 +18,8 @@ class LoginViewController: UIViewController {
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
+    
+    weak var delegate: LoginViewControllerDelegate?
     
     var username: String? {
         return loginView.usernameTextField.text
@@ -127,6 +133,7 @@ extension LoginViewController {
         
         if username == "Kieran" && password == "Welcome" {
             signInButton.configuration?.showsActivityIndicator = true
+            delegate?.didLogin()
         } else {
             configureView(withMessage: "Incorrect username / password")
         }
